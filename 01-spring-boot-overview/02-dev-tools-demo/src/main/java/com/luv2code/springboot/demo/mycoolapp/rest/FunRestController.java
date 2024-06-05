e11cd8dd3b1c664eadf0f9f469f51f10cdeffd5a
theopmw <theowrightdev@gmail.com>
2024-06-05 17:27:08 +0100

    expose /workout endpoint

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -12,4 +12,11 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World";
     }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
 }
