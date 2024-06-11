0a666b5e585ba373b17769349474420867b779e5
theopmw <theowrightdev@gmail.com>
2024-06-11 18:01:26 +0100

    add lazy annotation to TrackCoach

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/TrackCoach.java
@@ -1,8 +1,10 @@
 package com.springcourse.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TrackCoach implements Coach{
 
     public TrackCoach() {
