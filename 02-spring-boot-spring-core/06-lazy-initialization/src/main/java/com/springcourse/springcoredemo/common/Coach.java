1dfa10430c7f34e9483f7915e759a069de097ccd
theopmw <theowrightdev@gmail.com>
2024-06-11 17:50:27 +0100

    add lazy initialization project

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package com.springcourse.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.springcourse.springcoredemo.rest;
+
+import com.springcourse.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // Define a private field for the dependency
+    private Coach myCoach;
+
+    // @Autowired annotation means we can give any method name here and Spring will use it for setter dependency injection
+    @Autowired
+    public void DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+        public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
