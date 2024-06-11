17cf72e72d2db8e368d20fac556c2e955dff6eb9
theopmw <theowrightdev@gmail.com>
2024-06-11 17:51:44 +0100

    remove primary initialization

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/TrackCoach.java
@@ -1,10 +1,8 @@
 package com.springcourse.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
