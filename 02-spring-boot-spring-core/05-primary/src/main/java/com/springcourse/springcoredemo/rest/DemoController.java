d05030db3a8b52584bab326185a4f408bc99fb38
theopmw <theowrightdev@gmail.com>
2024-06-11 17:40:23 +0100

    add primary annotation project

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/springcourse/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.springcourse.springcoredemo.rest;
+
+import com.springcourse.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // Define a private field for the dependency
+    private Coach myCoach;
+
+    // @Autowired annotation means we can give any method name here and Spring will use it for setter dependency injection
+    @Autowired
+    public void DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+        public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
