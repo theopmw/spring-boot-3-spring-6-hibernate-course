1cdd8af8cd5ba66b82079393e7ecc1d135c36dbc
theopmw <theowrightdev@gmail.com>
2024-06-14 12:47:24 +0100

    add crud project

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/test/java/com/springcourse/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
