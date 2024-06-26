08b41d0c6088fbadf686813504c4a30221947095
theopmw <theowrightdev@gmail.com>
2024-06-26 16:05:32 +0100

    start student DAO implementation

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,10 @@
+package com.springcourse.cruddemo.dao;
+
+import com.springcourse.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+//    Define save method
+    void save(Student theStudent);
+
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,29 @@
+package com.springcourse.cruddemo.dao;
+
+import com.springcourse.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+
+//    Define field for entity manager
+    private EntityManager entityManager
+
+//    Inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+//    Implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+//        Saves student to db
+        entityManager.persist(theStudent);
+    }
+}
