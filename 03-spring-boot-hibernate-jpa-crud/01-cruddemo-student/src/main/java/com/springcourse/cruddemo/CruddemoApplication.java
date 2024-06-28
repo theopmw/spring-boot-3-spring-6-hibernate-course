56f6996cac688191db5e0a35d8eabb5c787e10ce
theopmw <theowrightdev@gmail.com>
2024-06-28 16:52:01 +0100

    save multiple students to db

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/CruddemoApplication.java
@@ -18,8 +18,25 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+//		Create multiple students
+		System.out.println("Creating 3 student objects...");
+		Student tempStudent1 = new Student("John", "Doe", "john@test.com");
+		Student tempStudent2 = new Student("Mary", "Lamb", "mary@test.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@test.com");
+
+//		Save the student objects
+		System.out.println("Saving the students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
