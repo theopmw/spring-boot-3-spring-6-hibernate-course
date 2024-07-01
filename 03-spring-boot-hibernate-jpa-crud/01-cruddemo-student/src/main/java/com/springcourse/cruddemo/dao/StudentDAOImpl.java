9a1778236c618b1dadc9c669103a6897931a4b36
theopmw <theowrightdev@gmail.com>
2024-07-01 19:19:00 +0100

    create query

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.springcourse.cruddemo.dao;
 
 import com.springcourse.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -30,5 +33,14 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+//        Create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+//        Return query results
+
     }
 }
