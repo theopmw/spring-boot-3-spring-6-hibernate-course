16f8d86ebce3e1a47c20ad6a74a6bac953c06a1e
theopmw <theowrightdev@gmail.com>
2024-07-03 08:56:52 +0100

    add method to update student in db

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/dao/StudentDAOImpl.java
@@ -55,4 +55,10 @@ public class StudentDAOImpl implements StudentDAO{
 //        Return query results
         return theQuery.getResultList();
     }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
 }
