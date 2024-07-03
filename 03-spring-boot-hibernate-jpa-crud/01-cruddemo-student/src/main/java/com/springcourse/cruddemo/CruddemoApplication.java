130d84a5ad5ccabe90261b0e8af8f2c92084a3c6
theopmw <theowrightdev@gmail.com>
2024-07-03 13:01:51 +0100

    update student firstName

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/springcourse/cruddemo/CruddemoApplication.java
@@ -28,8 +28,29 @@ public class CruddemoApplication {
 
 //			queryForStudents(studentDAO);
 
-			queryForStudentsByLastName(studentDAO);
+//			queryForStudentsByLastName(studentDAO);
+			
+			updateStudent(studentDAO);
 		};
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+
+//		Retrieve student based on id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+		System.out.println("Original student: " + myStudent);
+
+//		Change first name to "Scooby"
+		System.out.println("Updating student...");
+		myStudent.setFirstName("Scooby");
+
+//		Update the student
+		studentDAO.update(myStudent);
+
+//		Display the updated student
+		System.out.println("Updated student: " + myStudent);
 	}
 
 	private void queryForStudentsByLastName(StudentDAO studentDAO) {
